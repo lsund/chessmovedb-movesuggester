259bf289bc83477e20b2264c43ce2c8bfa3f31b1
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-10-11 20:09:31 +0200

    Add json parsing and cli parsing

--- build.sbt
+++ build.sbt
@@ -6,3 +6,12 @@ version := "1.0.0"
 // Dependencies
 libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
 libraryDependencies += "org.apache.kafka" %% "kafka" % "2.3.0"
+libraryDependencies += "com.github.scopt" %% "scopt" % "4.0.0-RC2"
+
+val circeVersion = "0.12.2"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,27 @@
+import scopt.OParser
+
+case class CliOptions(
+    moves: String
+) {
+  override def toString = s"CliOptions[$moves]"
+}
+
 object Main extends App {
-  println("Hello world")
+  val builder = OParser.builder[CliOptions]
+  val optsparser = {
+    import builder._
+    OParser.sequence(
+      programName("scopt"),
+      head("scopt", "4.x"),
+      builder
+        .opt[String]('p', "pgn")
+        .action((x, c) => c.copy(moves = x))
+        .text("Moves to base suggestion from")
+    )
+  }
+  OParser.parse(optsparser, args, CliOptions("e4 e5")) match {
+    case Some(CliOptions(moves)) =>
+      println(moves)
+    case _ => ;
+  }
 }
